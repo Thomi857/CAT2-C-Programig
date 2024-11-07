fe75a460f06b71edfc38c59af82146e923eed2ac
CT101/G/22726/24 CliffLinus Thomi Kiragu. <thomilinus5@gmail.com>
2024-11-07 22:30:20 +0300

    Create CAT2_Q1.c

---
+++ CAT2_Q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct employee {
+    char name[25];
+    int id;
+    char department[20];
+    float salary;
+    char email[50];
+};
+
+int main()
+{
+    struct employee emp;
+
+    strcpy(emp.name, "John Doe");
+    emp.id = 12345;
+    strcpy(emp.department, "Human Resources");
+    emp.salary = 55000.50;
+    strcpy(emp.email, "john.doe@company.com");
+
+    printf("Name: %s\n", emp.name);
+    printf("ID: %d\n", emp.id);
+    printf("Department: %s\n", emp.department);
+    printf("Salary: %.2f\n", emp.salary);
+    printf("Email: %s\n", emp.email);
+
+    return 0;
+}
